5d20628025e6210b6e82c4e92ce726bc79af85c2
Chloe Hill <chloehill333@gmail.com>
2023-03-16 23:31:36 +0000

    Combining nav and search input section - not completed nav but start

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import "./nav.css";
 import Logo from "../../assets/images/rapidrecipes.png";
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav bg-white">
-        <div class="h-16 mx-auto px-5 flex items-center justify-between container mx-auto ">
-          <div className="">
-            <img className="" src={Logo} alt="logo" />
-          </div>
+        <div class="mx-auto px-5 flex items-center justify-between container mx-auto ">
+            <img className="w-50 h-50" src={Logo} alt="logo" />
           <ul className="flex items-center gap-6">
             <li>
               <a className="hover:text-green-600 transition-colors transition ease-in-out delay-100" href="#search">
