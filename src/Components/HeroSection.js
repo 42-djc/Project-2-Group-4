d7fc9daa0c13d4b3b6bd148d99340952176bc251
Chloe Hill <chloehill333@gmail.com>
2023-03-21 21:34:54 +0000

    editing size of hero section and editing h3 content instead of default lorem text

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <div className="bg-gray-900 py-12 px-4 sm:px-6 lg:px-8" style={{ backgroundColor: '#F7BC00' }}>
+    <div className="bg-gray-900 py-4 px-4 sm:px-6 lg:px-8" style={{ backgroundColor: '#F7BC00' }}>
       <div className="max-w-7xl mx-auto text-left" style={{ backgroundColor: '#F7BC00' }}>
-      <h2 className="text-4xl font-bold mb-4" style={{ color: "#147866" }}>Meal prep doesn't</h2>
-
-      <h2 className="text-4xl font-bold mb-4" style={{ color: "#147866" }}>
-        have to be hard
-        </h2>
-        <h3 className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum mauris vel augue scelerisque, nec hendrerit ipsum gravida. Aliquam quis sapien quam. Curabitur auctor tristique justo, ut sodales nulla laoreet vitae.</h3>
+      <h2 className="text-center text-3xl font-bold mb-4" style={{ color: "#147866" }}>Meal prep doesn't have to be hard...</h2>
+        <h3 className="text-center text-white">Got a few slices of leftover bread, an odd onion in your cupboard and some milk in the fridge? Our recipe finder tool will show you all the things you can make, so none of your food goes to waste, with only a few added ingredients needed.</h3>
       </div>
     </div>
   );
