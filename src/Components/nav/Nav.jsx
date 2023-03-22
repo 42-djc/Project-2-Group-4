47674503fdbfa0ccae03f39eeef9b0d12461d895
Chloe Hill <chloehill333@gmail.com>
2023-03-22 18:30:36 +0000

    adding spacing to tailwind config for logo sizing

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -6,16 +6,13 @@ const Nav = () => {
     <nav>
       <div className="nav bg-white">
         <div class="mx-auto px-5 flex items-center justify-between container mx-auto ">
-            <img className="w-50 h-50" src={Logo} alt="logo" />
+          <div className="relative bg-red-500 pb-2/3">
+            <img className="h-26 w-full object-cover" src={Logo} alt="logo" />
+            </div>
           <ul className="flex items-center gap-6">
             <li>
               <a className="hover:text-green-600 transition-colors transition ease-in-out delay-100" href="#search">
                 Recipe Search
-              </a>
-            </li>
-            <li>
-              <a className="hover:text-green-600 transition-colors transition ease-in-out delay-100" href="#recipes">
-                Favourite Recipes
               </a>
             </li>
             <li>
