c2e3b434180ab587ce2ce9bc653b18f8d1e33c7a
42-djc <118062286+42-djc@users.noreply.github.com>
2023-03-22 19:08:11 +0000

    updated file pathways

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <div className="bg-gray-900 py-12 px-4 sm:px-6 lg:px-8" style={{ backgroundColor: '#F7BC00' }}>
+    <div className="bg-gray-900 py-12 px-4 sm:px-6 lg:px-8 shadow-sm" style={{ backgroundColor: '#F7BC00' }}>
       <div className="max-w-7xl mx-auto text-left" style={{ backgroundColor: '#F7BC00' }}>
       <h2 className="text-4xl font-bold mb-4" style={{ color: "#147866" }}>Meal prep doesn't</h2>
 
