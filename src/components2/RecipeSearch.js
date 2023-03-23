2f363552460a0df22ab71153fe0b5acfff63c97d
Andrew Styles <andystyles@hotmail.co.uk>
2023-03-23 19:15:05 +0000

    final styling tweaks, trimmed health options down

--- src/components2/RecipeSearch.js
+++ src/components2/RecipeSearch.js
@@ -69,9 +69,9 @@ import { GrLinkNext } from 'react-icons/gr'
       <div className='h-auto flex justify-center'> 
       <section className='m-0 w-full h-auto'>
         <form onSubmit={handleSubmit} className="flex justify-center h-auto pb-8 gap-4 bg-white rounded-lg">
-          <label className="font block text-gray-800 relative">
+          <label className="font block text-gray-800 relative ">
             Enter recipe keywords such as "chicken", "dessert", etc.
-          <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="bg-green-50 rounded-lg p-2 mt-2 w-full border-4 border-yellow-500 text-gray-800 shadow-md" placeholder="search"/>
+          <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className="bg-green-50 hover:bg-green-200 rounded-lg p-2 mt-2 w-full border-4 border-yellow-500 text-gray-800 custom-scrollbar shadow-md" placeholder="search"/>
             <button type="submit" className="absolute right-1 top-9 bg-green-500 text-white py-2 px-4 rounded-l-lg rounded-r-md" style={{ backgroundColor: '#147866' }}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-100" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M14.986 14.986a.75.75 0 0 1-1.061 0l-3.124-3.124a5.25 5.25 0 1 1 1.06-1.06l3.125 3.124a.75.75 0 0 1 0 1.061zm-5.5-1.5a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" clipRule="evenodd" />
@@ -81,31 +81,20 @@ import { GrLinkNext } from 'react-icons/gr'
 
           <label className="text-gray-800 font block">
             Select health requirement:
-            <select value={health} onChange={(e) => setHealth(e.target.value)} className="bg-green-50 rounded-lg p-2 mt-2 w-full border-4 border-yellow-500 text-gray-800 shadow-md">
+            <select value={health} onChange={(e) => setHealth(e.target.value)} className="bg-green-50 rounded-lg p-2 mt-2 w-full border-4 border-yellow-500 text-gray-800 appearance-none bg-no-repeat hover:bg-green-200 custom-scrollbar shadow-md">
               <option value="">Please select one or leave blank</option>
-              <option value="alcohol-cocktail"> alcohol-cocktail </option>
               <option value="alcohol-free"> alcohol-free </option>
-              <option value="celery-free"> celery-free </option>
               <option value="crustacean-free"> crustacean-free </option>
               <option value="dairy-free"> dairy-free </option>
-              <option value="DASH"> DASH </option>
               <option value="egg-free"> egg-free </option>
               <option value="fish-free"> fish-free </option>
-              <option value="fodmap-free"> fodmap-free </option>
               <option value="gluten-free"> gluten-free </option>
               <option value="immuno-supportive"> immuno-supportive </option>
               <option value="keto-friendly"> keto-friendly </option>
-              <option value="kidney-friendly"> kidney-friendly </option>
               <option value="kosher"> kosher </option>
-              <option value="low-fat-abs"> low-fat-abs </option>
-              <option value="low-potassium"> low-potassium </option>
               <option value="low-sugar"> low-sugar </option>
-              <option value="lupine-free"> lupine-free </option>
               <option value="Mediterranean"> Mediterranean </option>
               <option value="mollusk-free"> mollusk-free </option>
-              <option value="mustard-free"> mustard-free </option>
-              <option value="no-oil-added"> no-oil-added </option>
-              <option value="paleo"> paleo </option>
               <option value="peanut-free"> peanut-free </option>
               <option value="pescatarian"> pescatarian </option>
               <option value="pork-free"> pork-free </option>
@@ -113,9 +102,6 @@ import { GrLinkNext } from 'react-icons/gr'
               <option value="sesame-free"> sesame-free </option>
               <option value="shellfish-free"> shellfish-free </option>
               <option value="soy-free"> soy-free </option>
-              <option value="sugar-conscious"> sugar-conscious </option>
-              <option value="sulfite-free"> sulfite-free </option>
-              <option value="tree-nut-free"> tree-nut-free </option>
               <option value="vegan"> vegan </option>
               <option value="vegetarian"> vegetarian </option>
               <option value="wheat-free"> wheat-free </option>
